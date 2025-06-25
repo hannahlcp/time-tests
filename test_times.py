10709fffcbd89feb1c1c6a31abbd5a1fef271ee5
Hannah CP <hannahcp@outlook.com>
2025-06-25 17:08:59 +0100

    pytest parametrize

--- test_times.py
+++ test_times.py
@@ -1,48 +1,94 @@
 import times
 from pytest import raises
+import pytest
+
 
 def test_times():
-    large = times.time_range("2010-01-12 10:00:00","2010-01-12 12:00:00")
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = times.compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    expected = [
+        ("2010-01-12 10:30:00", "2010-01-12 10:37:00"),
+        ("2010-01-12 10:38:00", "2010-01-12 10:45:00"),
+    ]
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "time_range_1, time_range_2, expected",
+    [
+        pytest.param(
+            (times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")),
+            (times.time_range("2010-01-13 10:00:00", "2010-01-13 12:00:00")),
+            ([]),
+            id="no overlap",
+        ),
+        pytest.param(
+            times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+            times.time_range("2010-01-13 10:00:00", "2010-01-13 12:00:00"),
+            ([]),
+            id="no overlap",
+        ),
+        pytest.param(
+            times.time_range("2025-06-25 00:00:00", "2025-06-25 00:10:00", 2, 60),
+            times.time_range("2025-06-25 00:05:00", "2025-06-25 00:15:00", 2, 60),
+            ([("2025-06-25 00:05:30", "2025-06-25 00:09:30")]),
+            id="several overlapping",
+        ),
+    ],
+)
+def test_times_notoverlapping_parametrize(time_range_1, time_range_2, expected):
+    result = times.compute_overlap_time(time_range_1, time_range_2)
+    assert result == expected
+
 
 def test_times_notoverlapping():
     t0 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     t1 = times.time_range("2010-01-13 10:00:00", "2010-01-13 12:00:00")
-    result = times.compute_overlap_time(t0,t1)
+    result = times.compute_overlap_time(t0, t1)
     expected = []
     assert result == expected
-    
+
+
 def test_times_several_overlapping():
     t0 = times.time_range("2025-06-25 00:00:00", "2025-06-25 00:10:00", 2, 60)
     t1 = times.time_range("2025-06-25 00:05:00", "2025-06-25 00:15:00", 2, 60)
-    result = times.compute_overlap_time(t0,t1)
-    expected = [('2025-06-25 00:05:30', '2025-06-25 00:09:30')]
+    result = times.compute_overlap_time(t0, t1)
+    expected = [("2025-06-25 00:05:30", "2025-06-25 00:09:30")]
     assert result == expected
+
 
 def test_times_several_overlapping2():
     t0 = times.time_range("2025-06-25 00:00:00", "2025-06-25 00:10:00", 4, 60)
     t1 = times.time_range("2025-06-25 00:05:00", "2025-06-25 00:20:00", 2, 60)
-    result = times.compute_overlap_time(t0,t1)
-    expected = [('2025-06-25 00:05:30', '2025-06-25 00:07:15'),("2025-06-25 00:08:15","2025-06-25 00:10:00")]
+    result = times.compute_overlap_time(t0, t1)
+    expected = [
+        ("2025-06-25 00:05:30", "2025-06-25 00:07:15"),
+        ("2025-06-25 00:08:15", "2025-06-25 00:10:00"),
+    ]
     assert result == expected
+
 
 def test_times_start_end_same_time():
     t0 = times.time_range("2025-06-25 00:00:00", "2025-06-25 00:10:00")
     t1 = times.time_range("2025-06-25 00:10:00", "2025-06-25 00:15:00")
-    result = times.compute_overlap_time(t0,t1)
+    result = times.compute_overlap_time(t0, t1)
     expected = []
     assert result == expected
 
+
 def test_times_backward():
-    with raises(ValueError, match = r"End time must be greater than start time"):
+    with raises(ValueError, match=r"End time must be greater than start time"):
         times.time_range("2025-06-25 00:05:00", "2025-06-23 00:15:00")
 
+
 def test_interval_larger_than_range():
-    with raises(ValueError, match = r"Gap between intervals must be less than the total time range"):
+    with raises(
+        ValueError,
+        match=r"Gap between intervals must be less than the total time range",
+    ):
         times.time_range("2025-06-25 00:00:00", "2025-06-25 00:00:10", 2, 20)
 
-# To generate coverage report, run:
-pytest --cov="times" --cov-report "html"
+
+# To generate coverage report, run on terminal:
+# pytest --cov="times" --cov-report "html"
