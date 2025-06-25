0247ea30056cf9006798bd4c4723db714058f27c
Hannah CP <hannahcp@outlook.com>
2025-06-25 10:46:39 +0100

    Suggested David edits to removing import and test_times function

--- times.py
+++ times.py
@@ -1,5 +1,4 @@
 import datetime
-import test_times
 
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
@@ -25,6 +24,5 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
-    test_times.test_times()
 
 
